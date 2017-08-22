898354ef9ac36263091be2cf19f0a52351c96853
Anton Kechashin <atk@sitecore.net>
2017-08-22 16:23:45 +0300

    Initial commit

---
+++ src/Sitecore.Support.8855/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.8855")]
+[assembly: AssemblyProduct("Sitecore.Support.8855")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.8855/XA/Feature/CreativeExchange/Pipelines/Export/PageProcessing/PageAssetExtract.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.Support.XA.Feature.CreativeExchange.Pipelines.Export.PageProcessing
+{
+    using System.Text.RegularExpressions;
+    using Sitecore.XA.Feature.CreativeExchange.Pipelines.Export.PageProcessing;
+
+    public class PageAssetExtract : Sitecore.XA.Feature.CreativeExchange.Pipelines.Export.PageProcessing.PageAssetExtract
+    {
+        protected override void ProcessLink(PageProcessingArgs args, string link, string tag, Match match)
+        {
+            if (!link.Contains("tel:"))
+            {
+                base.ProcessLink(args, link, tag, match);
+            }
+        }
+    }
+}
